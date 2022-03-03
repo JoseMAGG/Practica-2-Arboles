fccefab8ebe7b219ec991819f7b94cdda4883a6c
JoseMAGG <jose.gonzalezg1@udea.edu.co>
2022-03-03 11:29:11 -0500

    Main

---
+++ src/models/Contact.java
@@ -0,0 +1,49 @@
+package models;
+
+public class Contact implements Comparable<Contact> {
+
+    private Long phoneNumber;
+    private String name;
+
+    public Contact(long phoneNumber, String name) {
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+    public Contact() {
+    }
+
+    
+
+    public Contact(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getUserId() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long userId) {
+        this.phoneNumber = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Contac [name=" + name + ", phoneNumber=" + phoneNumber + "]";
+    }
+
+    @Override
+    public int compareTo(Contact o) {
+
+        return this.phoneNumber.compareTo(o.phoneNumber);
+    }
+
+}
