67641d63b4aa0cb9f22837f5cd2e314c2b038068
Juan <Arismendy>
2022-03-05 12:14:47 -0500

    adding indentificador de llamadas class

---
+++ src/bussiness/avl/IdentificadorDeLlamadas.java
@@ -0,0 +1,26 @@
+
+package bussiness.avl;
+
+import models.Contact;
+
+class IdentificadorDeLlamadas {
+
+    public static void creteTreeFromFile() {
+
+    }
+
+    public static Contact searchContact(String number) {
+
+        return null;
+
+    }
+
+    public static void addContact(Contact contact) {
+
+    }
+
+    public static void avlToFile() {
+
+    }
+
+}
