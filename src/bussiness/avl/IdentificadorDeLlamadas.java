11ca58e9911432ea715bde497d0837c8b82ca4d0
Juan <Arismendy>
2022-03-05 13:47:05 -0500

    identificador de llamadas implemented

--- src/bussiness/avl/IdentificadorDeLlamadas.java
+++ src/bussiness/avl/IdentificadorDeLlamadas.java
@@ -1,26 +1,93 @@
 
 package bussiness.avl;
 
+import java.io.IOException;
+import arbol.binario.listaligada.busqueda.avl.ArbolAVL;
+import bussiness.util.AvlTo;
+import bussiness.util.FileTo;
 import models.Contact;
 
-class IdentificadorDeLlamadas {
+public class IdentificadorDeLlamadas {
 
-    public static void creteTreeFromFile() {
+    private ArbolAVL<Contact> directorio;
+
+    public IdentificadorDeLlamadas() {
+    }
+
+    public IdentificadorDeLlamadas(ArbolAVL<Contact> directorio) {
+        this.directorio = directorio;
+    }
+
+    public void creteAvlFromFile(String path) throws IOException {
+
+        this.directorio = FileTo.agenda(path); // crear el directorio general
 
     }
 
-    public static Contact searchContact(String number) {
+    public void creteAvlFromFile() throws IOException {
 
-        return null;
-
-    }
-
-    public static void addContact(Contact contact) {
+        this.directorio = FileTo.agenda("src/contactos.txt"); // crear el directorio general
 
     }
 
-    public static void avlToFile() {
+    public Contact searchContact(String number) {
 
+        Contact contact = new Contact((Long.valueOf(number)));
+        Contact foundContact = (Contact) this.directorio.buscar(contact).getDato();// ejemplo buscar
+
+        return foundContact;
+
+    }
+
+    public Contact addContact(Contact contact) {
+
+        return (Contact) this.directorio.insertarDato(contact).getDato();
+
+    }
+
+    public void avlToFile() throws IOException {
+
+        AvlTo.txtFile(this.directorio);
+
+    }
+
+    public void avlToFile(String path) throws IOException {
+
+        AvlTo.txtFile(this.directorio);
+
+    }
+
+    public ArbolAVL<Contact> getDirectorio() {
+        return directorio;
+    }
+
+    public void setDirectorio(ArbolAVL<Contact> directorio) {
+        this.directorio = directorio;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((directorio == null) ? 0 : directorio.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        IdentificadorDeLlamadas other = (IdentificadorDeLlamadas) obj;
+        if (directorio == null) {
+            if (other.directorio != null)
+                return false;
+        } else if (!directorio.equals(other.directorio))
+            return false;
+        return true;
     }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,8 +1,7 @@
 import arbol.binario.listaligada.busqueda.avl.ArbolAVL;
-
+import bussiness.util.FileTo;
 import models.Contact;
-
-import util.FileTo;
+import bussiness.avl.IdentificadorDeLlamadas;
 
 //3. Conocer que personas están asociados a una característica particular en uno de sus genes.
 
@@ -13,14 +12,25 @@ public class main {
      * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
-        // String inorden = "gehicbfdajklnmop";
-        // String preorden = "abceghidflkjmnop";
 
-        ArbolAVL<Contact> contactos;
-        contactos = FileTo.agenda("src/contactos.txt"); // crear el directorio general
-        System.out.println(contactos.toString());
-        Contact contact = new Contact((Long.valueOf("3148284339")));
-        System.out.println((contactos.buscar(contact)));// ejemplo buscar
+        IdentificadorDeLlamadas identificador = new IdentificadorDeLlamadas();
+
+        identificador.creteAvlFromFile();
+        Contact foundContact = identificador.searchContact("3148284339");
+        System.out.println("contacto encontrado: " + foundContact);
+
+        System.out.println("Contacto insertado" + (identificador.addContact(new Contact("ANA SOFIA", "123456"))));
+        foundContact = identificador.searchContact("123456");
+        System.out.println("contacto encontrado: " + foundContact);
+
+        identificador.avlToFile();
+
+        // ArbolAVL<Contact> contactos;
+        // contactos = FileTo.agenda("src/contactos.txt"); // crear el directorio
+        // general
+        // System.out.println(contactos.toString());
+        // Contact contact = new Contact((Long.valueOf("3148284339")));
+        // System.out.println((contactos.buscar(contact)));// ejemplo buscar
 
     }
 
@@ -38,12 +48,13 @@ public class main {
 // 1. 302345679 : 301987653 -> ojo con este que ya existía
 // se pueden ingresar nuevos contactos de nivel 1 o de nivel 2 en adelante
 
-
-//1. buscar en el arbol AVL (Identificador de llamadas) -> JD
-//2. Lista de contactos del arbol de contactos ejecutivos Arbol binario  -> LM imprimir los contactos(hijos) de un contacto
-//3. ingresar contacto nivel 1 en arbol de contactos ejecutivos  -> JM
-//4. Ingresar un nuevo contacto en el nivel 2 o 3  en arbol de contactos ejecutivos -> JM
-//5. Ingresar un nuevo contacto  en el arbol AVL (Identificador de llamadas) ->JD
-//6. Guardar los arboles en archivos de texto txt  ->  LM 
-//7. Vista y menus -> LT
-
+// 1. buscar en el arbol AVL (Identificador de llamadas) -> JD
+// 2. Lista de contactos del arbol de contactos ejecutivos Arbol binario -> LM
+// imprimir los contactos(hijos) de un contacto
+// 3. ingresar contacto nivel 1 en arbol de contactos ejecutivos -> JM
+// 4. Ingresar un nuevo contacto en el nivel 2 o 3 en arbol de contactos
+// ejecutivos -> JM
+// 5. Ingresar un nuevo contacto en el arbol AVL (Identificador de llamadas)
+// ->JD
+// 6. Guardar los arboles en archivos de texto txt -> LM
+// 7. Vista y menus -> LT
