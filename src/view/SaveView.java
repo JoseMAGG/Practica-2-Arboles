aeaf8ea0cec1de8b334266029b3742a1738af7be
JoseMAGG <jose.gonzalezg1@udea.edu.co>
2022-03-07 10:54:39 -0500

    Listo guardar empresariales

--- src/view/SaveView.java
+++ src/view/SaveView.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import arbol.nario.binariolistaligada.ArbolContactosEjecutivos;
 import bussiness.avl.IdentificadorDeLlamadas;
 import models.Contact;
+import bussiness.util.EjecutivosTo;
 
 public class SaveView {
     public static void index(IdentificadorDeLlamadas identificador, ArbolContactosEjecutivos<Contact> ejecutivos) throws IOException {
         identificador.avlToFile();
+        EjecutivosTo.txtFile(ejecutivos, "src/empresariales.txt");
         //ejecutivos.binariosToFile();
         System.out.println("Árboles guardados con éxito");
     }
